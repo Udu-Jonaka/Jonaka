743c188d602f029a91a56efc6706b1e5b43ec150
Udu Jonaka Nnanna <udunnanna0@gmail.com>
2025-12-05 17:04:51 +0100

    christmas decor

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 5100;
+const PORT = 4000;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));  // replaces body-parser
