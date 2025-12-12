f95977b69a3ba9f01824aeed98884a4d1c6680d1
Udu Jonaka Nnanna <udunnanna0@gmail.com>
2025-12-13 00:26:26 +0100

    add sitemap v2

--- app.js
+++ app.js
@@ -25,13 +25,47 @@ app.get('/', (req, res) => {
 
 // If files are in a public folder:
 app.get('/sitemap.xml', (req, res) => {
-  res.type('application/xml');
-  res.sendFile(__dirname + '/public/sitemap.xml');
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://jonaka-seven.vercel.app/</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://jonaka-seven.vercel.app/#skills</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://jonaka-seven.vercel.app/#portfolio</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://jonaka-seven.vercel.app/#contact</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+</urlset>`;
+
+  res.header('Content-Type', 'application/xml');
+  res.send(sitemap);
 });
 
+// Robots.txt route
 app.get('/robots.txt', (req, res) => {
+  const robots = `User-agent: *
+Allow: /
+
+Sitemap: https://jonaka-seven.vercel.app/sitemap.xml`;
+  
   res.type('text/plain');
-  res.sendFile(__dirname + '/public/robots.txt');
+  res.send(robots);
 });
 
 // Start server
